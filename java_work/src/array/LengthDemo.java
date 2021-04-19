f211b0ab3f9d467989fe93c0ade0540b8c1b3e40
dongkyung <ckldk91m@naver.com>
2021-04-19 13:58:35 +0900

    4.19

--- java_work/src/array/LengthDemo.java
+++ java_work/src/array/LengthDemo.java
@@ -1,11 +1,17 @@
 package array;
 
+import java.util.Arrays;
+
 public class LengthDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String[] classgroup = new String [4];
 		System.out.println(classgroup.length);
+		classgroup[0] = "hi";
+		classgroup[1] = "hi2";
+				classgroup[2] = "hi3";
+		System.out.println(Arrays.toString(classgroup));
 
 	}
 
