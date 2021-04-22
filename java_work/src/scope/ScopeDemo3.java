78f19663b36da7bc6fe7e00ee38313ba6e511e91
User <ckldk91m@naver.com>
2021-04-22 10:29:33 +0900

    4/22

---
+++ java_work/src/scope/ScopeDemo3.java
@@ -0,0 +1,22 @@
+package scope;
+
+public class ScopeDemo3 {
+	
+	static int i;
+	
+	static void a() {
+		int i = 0;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for (i=0; i<5;i++) {
+			a();
+			System.out.println(i);
+		}
+		System.out.println(i);
+
+	}
+
+}
