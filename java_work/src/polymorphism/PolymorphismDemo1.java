10b022f352d7fdd0769be6cd05890a8b52a5c611
User <ckldk91m@naver.com>
2021-05-04 17:40:42 +0900

    210504

--- java_work/src/polymorphism/PolymorphismDemo1.java
+++ java_work/src/polymorphism/PolymorphismDemo1.java
@@ -1,10 +1,24 @@
 package polymorphism;
 
 class A {
-	
+	public String x(){
+		return "A.X";
+	}
 }
 class B extends A{
+	public String x() {
+		return "B.X";
+	}
+	public String y() {
+		return "Y";
+	}
 	
+}
+
+class B2 extends A {
+	public String x() {
+		return "B2.x";
+	}
 }
 
 public class PolymorphismDemo1 {
@@ -12,6 +26,12 @@ public class PolymorphismDemo1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		A obj = new B();
+		A obj2 = new B2();
+		
+		obj.x();
+//		obj.y();
+		System.out.println(obj.x());
+		System.out.println(obj2.x());
 		
 
 	}
