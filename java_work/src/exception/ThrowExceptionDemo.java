720c1aff98a9112f19db09171b1bf207b68014e6
L-dongkyung <ckldk91m@naver.com>
2021-05-18 12:01:57 +0900

    0518

---
+++ java_work/src/exception/ThrowExceptionDemo.java
@@ -0,0 +1,41 @@
+package exception;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+class B{
+	void run() {
+		BufferedReader bReader = null;
+		String input = null;
+		try {
+			bReader =  new BufferedReader(new FileReader("out.txt"));
+		}catch(FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		try {
+			input = bReader.readLine();
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println(input);
+	}
+}
+class C{
+	void run() {
+		B b = new B();
+		b.run();
+	}
+}
+
+public class ThrowExceptionDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		C c = new C();
+		c.run();
+
+	}
+
+}
